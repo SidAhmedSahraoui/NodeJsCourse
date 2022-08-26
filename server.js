33836cfb80c3647671a0835441d54306ffabfd87
Sid Ahmed <s.sahraoui@esi-sba.dz>
2022-08-26 18:06:33 +0100

    friday commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: graphqlSchema,
+      rootValue: graphqlResolver,
+      graphiql: true
+    })
+  );
 
 app.listen(3000);
