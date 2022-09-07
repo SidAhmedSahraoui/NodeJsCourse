6c4adaf1bfaf23c63c143e84ed51dc9bc93a0feb
Sid Ahmed <s.sahraoui@esi-sba.dz>
2022-09-07 11:24:01 +0100

    ----

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 // components
-import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
 
 class App extends Component {
   render() {
--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import Input from '../../components/Input/Input';
-import Button from '../../components/Button/Button';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
 import { required, length, email } from '../../util/validators';
 import Auth from './Auth';
 
