572f60e1da70b7b27069ce383289f8e37e18aa89
Sid Ahmed <s.sahraoui@esi-sba.dz>
2022-09-07 11:31:56 +0100

    ----

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,8 @@
-import React, { Component } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.css";
-// components
-import Login from "./components/Auth/Login";
-import Signup from "./components/Auth/Signup";
-
-class App extends Component {
-  render() {
-    return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  }
+import React from 'react';
+const App = () => {
+  return ( 
+    <>7</>
+   );
 }
-
+ 
 export default App;
