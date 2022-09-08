b9a2a7b00565b5b47211f402eedaf95248cec513
Sid Ahmed <s.sahraoui@esi-sba.dz>
2022-09-08 10:23:20 +0100

    ----

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const s = "123456";
+/*const s = "123456";
 function despite(str) {
   const arr = [];
   let s = "";
@@ -13,4 +13,31 @@ function despite(str) {
   }
   console.log(arr);
 }
-despite(s);
+despite(s);*/
+/*const s = 1000
+function devop(str) {
+  let arr = []
+  const all = []
+  for(let i=1 ; i<str ; i++) {
+    arr = []
+    for(let k=1 ; k<i+1 ; k++){
+      if(i%k === 0) {
+        arr.push(k)
+      }
+    }
+    if(arr.length === 2){
+      all.push(i)
+    }
+  }
+  console.log(all);
+  console.log(all.length);
+}
+devop(s)*/
+const ex = 5000
+function dev(arg) {
+  for(let i=0 ; i<arg ; i++){
+    
+  }
+  console.log("ee"); 
+}
+dev()
